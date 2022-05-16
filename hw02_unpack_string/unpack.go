86687c0d416d4c756a7049196cb44e6bdde9f3ad
savproga <savproga@mail.ru>
2022-05-16 16:40:57 +0300

    HW2 is completed

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,45 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strconv"
+	"strings"
+	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(input string) (string, error) {
+	var builder strings.Builder
+	var flagEscape bool
+	for i, char := range input {
+		switch {
+		case string(char) == `\` && !flagEscape:
+			flagEscape = true
+		case flagEscape:
+			builder.WriteRune(char)
+			flagEscape = false
+		case unicode.IsDigit(char) && len(builder.String()) > 0:
+			number, _ := strconv.Atoi(string(char))
+
+			if number == 0 {
+				if !unicode.IsDigit(rune(input[i-1])) {
+					str := []rune(builder.String())
+					str = str[:len(str)-1]
+					builder.Reset()
+					builder.WriteString(string(str))
+				} else {
+					return "", ErrInvalidString
+				}
+			} else {
+				w := []rune(builder.String())
+				w = w[len(w)-1:]
+				builder.WriteString(strings.Repeat(string(w), number-1))
+			}
+		case !unicode.IsDigit(char):
+			builder.WriteRune(char)
+		default:
+			return "", ErrInvalidString
+		}
+	}
+	return builder.String(), nil
 }
